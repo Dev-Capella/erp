533434ff8295d5aded04ac53888d4d96128d250e
mahmutcopoglu <mahmutcopoglu@gmail.com>
2023-12-02 23:10:18 +0300

    main

--- webapp/src/main/java/com/capella/controller/v1/unitofmeasure/UnitOfMeasureController.java
+++ webapp/src/main/java/com/capella/controller/v1/unitofmeasure/UnitOfMeasureController.java
@@ -8,10 +8,7 @@ import com.capella.facade.unitofmeasure.UnitOfMeasureFacade;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController("unitOfMeasureControllerV1")
 @RequestMapping(ControllerMappings.VERSION_V1 + ControllerMappings.UNITOFMEASURE)
@@ -21,10 +18,36 @@ public class UnitOfMeasureController {
 
     protected final UnitOfMeasureFacade unitOfMeasureFacade;
 
-    @PostMapping(ControllerMappings.UNITOFMEASURE)
+    @PostMapping
     public ServiceResponseData save(@Validated @RequestBody UnitOfMeasureData unitOfMeasureData){
         log.info("Inside save of UnitOfMeasureController",unitOfMeasureData);
         unitOfMeasureFacade.save(unitOfMeasureData);
+        var response = new ServiceResponseData();
+        response.setStatus(ProcessStatus.SUCCESS);
+        return response;
+    }
+    @GetMapping(ControllerMappings.CODE)
+    public ServiceResponseData get(@PathVariable String code){
+        log.info("Inside get of UnitOfMeasureController",code);
+        var unitOfMeasureData = unitOfMeasureFacade.get(code);
+        var response = new ServiceResponseData();
+        response.setStatus(ProcessStatus.SUCCESS);
+        response.setData(unitOfMeasureData);
+        return response;
+    }
+    @GetMapping
+    public ServiceResponseData getAll(){
+        log.info("Inside getAll of UnitOfMeasureController");
+        var unitOfMeasureDatas = unitOfMeasureFacade.getAll();
+        var response = new ServiceResponseData();
+        response.setStatus(ProcessStatus.SUCCESS);
+        response.setData(unitOfMeasureDatas);
+        return response;
+    }
+    @DeleteMapping(ControllerMappings.CODE)
+    public ServiceResponseData delete(@PathVariable String code){
+        log.info("Inside delete of UnitOfMeasureController",code);
+        unitOfMeasureFacade.delete(code);
         var response = new ServiceResponseData();
         response.setStatus(ProcessStatus.SUCCESS);
         return response;
